07b953aa8ec0a302fb8e7f59fbd0eccf85b7654f
ericzlmd <ericzlmd@gmail.com>
2018-02-21 00:38:21 -0500

    Added form validation for feedback.html, register.html and login.html

--- js/main.js
+++ js/main.js
@@ -6,10 +6,12 @@ window.onload = function() {
 $(document).ready(function() {
   $(".error").hide();
   
-  $(".btn-submit").click(function() {
+
+  // login.html form check
+  $("#login-btn").click(function() {
     $(".error").hide();
 
-    // check email
+    // email
     var email = $("#email").val();
     if(email == "") {
       $("label#lgn-email-error").show();
@@ -17,7 +19,7 @@ $(document).ready(function() {
       return false;
     }
 
-    // check password
+    // password
     var pass = $("#password").val();
     if(pass == "") {
       $("label#lgn-pass-error").show();
@@ -25,12 +27,82 @@ $(document).ready(function() {
       return false;
     }
   });
+
+
+  // register.html form check
+  $("#register-btn").click(function() {
+    $(".error").hide();
+
+    // first name
+    var fname = $("#firstname").val();
+    if(fname == "") {
+      $("label#reg-firstname-error").show();
+      $("input#firstname").focus();
+      return false;
+    }
+
+    // last name
+    var lname = $("#lastname").val();
+    if(lname == "") {
+      $("label#reg-lastname-error").show();
+      $("input#lastname").focus();
+      return false;
+    }
+
+    // email
+    var email = $("#email").val();
+    if(email == "") {
+      $("label#reg-email-error").show();
+      $("input#email").focus();
+      return false;
+    }
+
+    // password
+    var pass = $("#password").val();
+    if(pass == "") {
+      $("label#reg-pass-error").show();
+      $("input#password").focus();
+      return false;
+    }
+
+  });
+
+  // feedback.html form validation
+  $("#feedback-btn").click(function() {
+    $(".error").hide();
+
+    // email
+    var email = $("#email").val();
+    if(email == "") {
+      $("label#fb-email-error").show();
+      $("input#email").focus();
+      return false;
+    }
+
+    // textarea
+    var txtarea = $("#fb-textarea").val();
+    if(txtarea == "") {
+      $("label#fb-textarea-error").show();
+      $("input#fb-textarea").focus();
+      return false;
+    }
+  });
 });
 
+// checkPasswordLength() - password user input must be > 8 characters
 function checkPasswordLength(pwd) {
   check = false;
   if(pwd.length >= 8) {
      check = true;
+  }
+  return check;
+}
+
+// checkNameEntryLength() - name entries on register.html should be > 3 characters
+function checkNameEntryLength(name) {
+  check = false;
+  if(name.length > 3) {
+    check = true;
   }
   return check;
 }
