720a0dae855552ac9b3045516d326a24d30195ec
Richard Dip <31774774+richarddip28@users.noreply.github.com>
2018-02-28 13:01:34 -0500

    Add flightboard,flightconfirm
    flight_board.html has 6 continually refreshing flight details
    
    flight_confirmation is the confirmation screen prior to booking it for the user

---
+++ js/flight_board.js
@@ -0,0 +1,110 @@
+//
+//Author  : Richard Dip
+//Date    : 02/27/2018
+//Details : Flight Board with 6 elements refreshing every 10 seconds
+//		    Clicking on the section alerts() information 
+//
+
+window.onload=init;
+
+function init(){
+	
+	populate();
+	
+	setInterval(function(){
+		populate();
+	},10000);
+		
+	
+	$("section").click(function(){
+		goToBooking(this.id);
+	})
+
+	
+}
+
+function goToBooking(i){
+
+	var dest = document.getElementById("destination"+i).innerHTML;
+	var date = document.getElementById("date"+i).innerHTML;
+	var time = document.getElementById("time"+i).innerHTML;
+	var cost = document.getElementById("points"+i).innerHTML;
+	
+	var  confirm_window = confirm("Confirmation\n\nDesination:"+ dest + "\nDate:" + date + "\nTime:" + time + "\nCost:" + cost);
+	
+	
+	//this block of code would send to server for flight_confirmation to fetch
+	if(confirm_window == true)
+		window.location.href = "flight_confirmation.html";
+	else
+		alert("lol bruh");
+	
+	
+	
+}
+
+
+function populate(){
+	
+	for(i=0;i<7;i++)
+	{
+		
+		getDestination(i);
+		getDate(i);
+		getTime(i);
+		getPoints(i);
+	
+	}
+}
+
+function getDestination(i){
+	
+	var num = Math.floor(Math.random()*20) + 1;
+	var destination = document.getElementById("destination"+i);
+	
+	var cities = [	"Toronto, Canada", "New York, United States", "Lisbon, Portugal", "Venice, Italy", "Cairo, Egypt", 
+					"Perth, Australia", "Paris, France", "Hong Kong, China", "Kingston, Jamaica", 
+					"England, United Kingdom", "Stockholm, Sweden", "Cancun, Mexico", "Seoul, South Korea", 
+					"Anthens, Greece", "Hanoi, Vietnam", "Helsinki, Finland", "Vancouver, Canada", "Bangkok, Thailand", 
+					"Tokyo, Japan", "Sao Paulo, Brazil", ];
+	
+	
+	destination.innerHTML = ""+cities[num];
+			
+	
+	
+}
+
+function getDate(i){
+	var num = Math.floor(Math.random()*3) + 1;
+	var date = document.getElementById("date"+i);
+	
+	var month = Math.floor(Math.random()*12) + 1;
+	var day = Math.floor(Math.random()*30) + 1;
+	var year = Math.floor(Math.random()*3) + 18;
+	
+
+	date.innerHTML = "" + month + "/" + day + "/20" + year; 
+		
+	
+}
+
+function getTime(i){
+	var num = Math.floor(Math.random()*3) + 1;
+	var time = document.getElementById("time"+i);
+	
+	var hour = Math.floor(Math.random()*23) + 1;
+	
+	time.innerHTML = "" + hour + ":00";
+	
+}
+
+function getPoints(i){
+	var num = Math.floor(Math.random()*3) + 1;
+	var points = document.getElementById("points"+i);
+	var x = Math.floor(Math.random()*5000) + 100;
+	
+	var cost = Math.round(x/100)*100;
+	
+	points.innerHTML = "" + cost + " pts";
+}
---
+++ js/flight_confirmation.js
@@ -0,0 +1,31 @@
+//
+//Author  : Richard Dip
+//Date    : 02/28/2018
+//Details : Flight confirmation screen
+//		    
+//
+
+window.onload = init;
+
+function init(){
+	
+	populate();
+	
+	
+}
+
+function populate(){
+	
+	var dest = document.getElementById("destination");
+	var date = document.getElementById("date");
+	var time = document.getElementById("time");
+	var cost = document.getElementById("points");
+	
+	//this function would pull from the server and populate fields
+	
+	
+	
+	
+	
+}
+
