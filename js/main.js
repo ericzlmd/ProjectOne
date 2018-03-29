52bd8a622c77fb56b52a418e12120bbfd5afa5bc
AdrienYKC <32147501+AdrienYKC@users.noreply.github.com>
2018-03-29 00:53:17 -0400

    Some changes
    Some changes
    -about.html
    -feedback.html

--- js/main.js
+++ js/main.js
@@ -171,6 +171,13 @@ function validatePassword(password) {
       $("input#email").focus();
       return false;
     }
+	
+	// radiofeed
+	var radiof = $("#radiofeed").val();
+    if(radiof == "") {
+      $("label#fb-radiofeed-error").show();
+	  return false;
+	}
 
     // textarea
     var txtarea = $("#fb-textarea").val();
